ddab69e774cc549d9b9e624591fae43a21b1ea24
Ed Mays <copperstarsystems@gmail.com>
2018-02-10 14:20:24 -0500

    Clean up GetData class, rename to Repository, remove unnecessary static methods from GetData, update GameOnController to use an instance of Repository instead of static implementation.

--- slnGameOn/Controllers/GameOnController.cs
+++ slnGameOn/Controllers/GameOnController.cs
@@ -1,80 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿//  --------------------------------------------------------------------------------------
+// slnGameOn.GameOnController.cs
+// 2018/02/09
+//  --------------------------------------------------------------------------------------
+
+using System;
 using System.Data;
+using System.Web.Mvc;
 using slnGameOn.db;
 using slnGameOn.Models;
 
 namespace slnGameOn.Controllers
 {
-	public class GameOnController : Controller
-	{
-		public ActionResult Browse()
-		{
-			return View();
-		}
+    public class GameOnController : Controller
+    {
+        public ActionResult Account()
+        {
+            var repository = new Repository();
+            var CustomerId = 4;
+            var Qry = "spGetAccountInfo @CustomerId=" + CustomerId;
+            var ds = repository.ReturnDataSet(Qry);
+            DataTable dt = ds.Tables[0], dt2 = ds.Tables[1];
+            var oc = new mCustomer(CustomerId);
+            oc.FirstName = ds.Tables[0].Rows[0]["FirstName"].ToString();
+            oc.LastName = ds.Tables[0].Rows[0]["LastName"].ToString();
+            oc.Gender = ds.Tables[0].Rows[0]["Gender"].ToString();
+            oc.DOB = ds.Tables[0].Rows[0]["DOB"].ToString();
+            oc.MaritalStatus = ds.Tables[0].Rows[0]["MaritalStatus"].ToString();
+            oc.EmailId = ds.Tables[0].Rows[0]["EmailId"].ToString();
+            oc.Email = ds.Tables[0].Rows[0]["Email"].ToString();
+            oc.AddressId = ds.Tables[0].Rows[0]["AddressId"].ToString();
+            oc.Address = ds.Tables[0].Rows[0]["Address"].ToString();
+            oc.CityStateZip = ds.Tables[0].Rows[0]["CityStateZip"].ToString();
+            oc.PassWord = ds.Tables[0].Rows[0]["PassWord"].ToString();
+            foreach (DataRow r in dt2.Rows)
+                oc.Phone.Add(new mPhone
+                             {
+                                 PhoneId = Convert.ToInt32(r["PhoneId"]),
+                                 TypeId = Convert.ToInt32(r["PhoneTypeId"]),
+                                 Type = r["PhoneType"].ToString(),
+                                 Phone = r["Phone"].ToString()
+                             });
+            ds.Clear();
+            return View(oc);
+        }
 
+        [HttpPost]
+        public ActionResult Account(string btSubmit)
+        {
+            var Ctr = "";
+            var ModelCust = new mCustomer(6);
+            return View("Account", ModelCust);
+        }
 
-		public ActionResult Cart()
-		{
-			return View();
-		}
-		public ActionResult OrderHistory()
-		{
-			return View();
-		}
-		public ActionResult Account()
-		{
-			int CustomerId = 4;
-			string Qry = "spGetAccountInfo @CustomerId=" + CustomerId.ToString();
-			DataSet ds = GetData.ReturnDataSet(Qry);
-			DataTable dt = ds.Tables[0], dt2 = ds.Tables[1];
-			var oc = new mCustomer(CustomerId);
-			oc.FirstName = ds.Tables[0].Rows[0]["FirstName"].ToString();
-			oc.LastName = ds.Tables[0].Rows[0]["LastName"].ToString();
-			oc.Gender = ds.Tables[0].Rows[0]["Gender"].ToString();
-			oc.DOB = ds.Tables[0].Rows[0]["DOB"].ToString();
-			oc.MaritalStatus = ds.Tables[0].Rows[0]["MaritalStatus"].ToString();
-			oc.EmailId = ds.Tables[0].Rows[0]["EmailId"].ToString();
-			oc.Email = ds.Tables[0].Rows[0]["Email"].ToString();
-			oc.AddressId = ds.Tables[0].Rows[0]["AddressId"].ToString();
-			oc.Address = ds.Tables[0].Rows[0]["Address"].ToString();
-			oc.CityStateZip = ds.Tables[0].Rows[0]["CityStateZip"].ToString();
-			oc.PassWord = ds.Tables[0].Rows[0]["PassWord"].ToString();
-			foreach (DataRow r in dt2.Rows)
-			{
-				oc.Phone.Add(new mPhone { PhoneId = Convert.ToInt32(r["PhoneId"]), TypeId = Convert.ToInt32(r["PhoneTypeId"]), Type = r["PhoneType"].ToString(), Phone = r["Phone"].ToString() });
-			}
-			ds.Clear();
-			return View(oc);			
-		}
+        public ActionResult Browse()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult Account(string btSubmit)
-		{
-			string Ctr = "";
-			var ModelCust = new mCustomer(6);	
-			return View("Account", ModelCust);
-		}
+        [HttpPost]
+        public ActionResult Browse(string btSubmit)
+        {
+            var ModelCust = new mCustomer(6);
+            return View("Cart", ModelCust);
+        }
 
+        public ActionResult Cart()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult Browse(string btSubmit)
-		{
-			var ModelCust = new mCustomer(6);			
-			return View("Cart", ModelCust);
-		}
-
-
-
-
-
-
-
-
-
-
-	}
+        public ActionResult OrderHistory()
+        {
+            return View();
+        }
+    }
 }
---
+++ slnGameOn/db/Repository.cs
@@ -0,0 +1,40 @@
+﻿//  --------------------------------------------------------------------------------------
+// slnGameOn.Repository.cs
+// 2018/02/09
+//  --------------------------------------------------------------------------------------
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace slnGameOn.db
+{
+    public class Repository
+    {
+        readonly string connectionString = WebConfigurationManager.ConnectionStrings["cnSql"].ConnectionString;
+
+        public DataSet ReturnDataSet(string query)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var dataAdapter = new SqlDataAdapter(command))
+                    {
+                        try
+                        {
+                            var data = new DataSet();
+                            connection.Open();
+                            dataAdapter.Fill(data);
+                            return data;
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
