eb8b4edc4fdbdbfda10c23ba49111d232060a394
Ed Mays <copperstarsystems@gmail.com>
2018-02-10 14:45:07 -0500

    Fix naming for custom.js, update _Layout.cshtml to reflect, remove duplicate script declarations in individual view files.

---
+++ slnGameOn/ScriptsCustom/site.js
@@ -0,0 +1,13 @@
+﻿function SetActivePage(page) {
+    var pages = ["Browse", "Cart", "OrderHistory", "Account"];
+    var navTabClass = "NavTab";
+    var bg1Class = "bg1";
+    $("#td" + page).addClass(navTabClass);
+    $("#td" + page).removeClass(bg1Class);
+    for (var i = 0; i < pages.length; i++) {
+        if (page !== pages[i]) {
+            $("#td" + pages[i]).addClass(bg1Class);
+            $("#td" + pages[i]).removeClass(navTabClass);
+        }
+    }
+}
