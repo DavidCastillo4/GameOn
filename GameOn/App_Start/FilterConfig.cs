932677b04f3a65acba837d8fff618fdfcc37ec7a
Ed Mays <copperstarsystems@gmail.com>
2018-02-14 14:20:47 -0500

    Move Global.asax logic to Startup.cs, remove Global.asax, Configure cookie authentication

---
+++ GameOn/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿//  --------------------------------------------------------------------------------------
+// GameOn.FilterConfig.cs
+// 2018/02/14
+//  --------------------------------------------------------------------------------------
+
+using System.Web.Mvc;
+
+namespace GameOn
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
