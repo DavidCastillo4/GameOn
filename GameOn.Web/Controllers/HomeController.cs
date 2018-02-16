f562b952beae9d0396842dc901fe38be0e2e7545
Ed Mays <copperstarsystems@gmail.com>
2018-02-16 15:17:41 -0500

    Introduce HomeController

---
+++ GameOn.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//  --------------------------------------------------------------------------------------
+// GameOn.Web.HomeController.cs
+// 2018/02/16
+//  --------------------------------------------------------------------------------------
+
+using System.Web.Mvc;
+
+namespace GameOn.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
