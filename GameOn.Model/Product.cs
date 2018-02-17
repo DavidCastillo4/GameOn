5e4401d91f50ab5cf011691d1effdf34e731e1ce
Ed Mays <copperstarsystems@gmail.com>
2018-02-16 19:58:37 -0500

    Introduce Model assembly, stub out Product class

---
+++ GameOn.Model/Product.cs
@@ -0,0 +1,18 @@
+﻿//  --------------------------------------------------------------------------------------
+// GameOn.Model.Product.cs
+// 2018/02/16
+//  --------------------------------------------------------------------------------------
+
+namespace GameOn.Model
+{
+    public class Product
+    {
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
