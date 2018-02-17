35084181473cc87ef075b4d53b506e51f36210ea
Ed Mays <copperstarsystems@gmail.com>
2018-02-16 20:33:35 -0500

    Remove Global.asax, implement startup.cs, introduce AutoFac as dependency injection container.

--- GameOn.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameOn.Web.MvcApplication" Language="C#" %>
---
+++ GameOn.Web/Startup.cs
@@ -0,0 +1,27 @@
+﻿//  --------------------------------------------------------------------------------------
+// GameOn.Web.Startup.cs
+// 2018/02/16
+//  --------------------------------------------------------------------------------------
+
+using System.Web.Mvc;
+using System.Web.Routing;
+using GameOn.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace GameOn.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            AreaRegistration.RegisterAllAreas();
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
