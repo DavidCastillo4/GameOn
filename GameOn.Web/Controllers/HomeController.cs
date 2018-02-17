816c66fe2f5d2487a1c494982ede454dfef13fdb
Ed Mays <copperstarsystems@gmail.com>
2018-02-16 19:56:03 -0500

    Update home page to reflect that "Browse" is the default action for this application.

--- GameOn.Web/Controllers/HomeController.cs
+++ GameOn.Web/Controllers/HomeController.cs
@@ -10,6 +10,9 @@ namespace GameOn.Web.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        // This is the default page for this controller.  Since this is
+        // the default controller for the application, this can be considered
+        // the 'home' page (i.e. what the user first sees when visiting the domain)
         public ActionResult Index()
         {
             return View();
