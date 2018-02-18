40f2365cc46c8bf8dd3ebf16079880152474a3d0
Ed Mays <copperstarsystems@gmail.com>
2018-02-18 12:19:51 -0500

    Stub out "Add to Cart" page

--- GameOn.Web/Controllers/CartController.cs
+++ GameOn.Web/Controllers/CartController.cs
@@ -4,12 +4,24 @@
 //  --------------------------------------------------------------------------------------
 
 using System.Web.Mvc;
+using GameOn.Repository;
 
 namespace GameOn.Web.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
+        readonly IRepository repository;
+
+        public CartController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Add(int productId)
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
