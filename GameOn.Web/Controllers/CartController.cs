d075a199e70f3be5af61a2602673d43d223d9e1c
Ed Mays <copperstarsystems@gmail.com>
2018-02-18 12:14:29 -0500

    Stub out cart controller

---
+++ GameOn.Web/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿//  --------------------------------------------------------------------------------------
+// GameOn.Web.CartController.cs
+// 2018/02/18
+//  --------------------------------------------------------------------------------------
+
+using System.Web.Mvc;
+
+namespace GameOn.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
